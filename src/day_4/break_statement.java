846e447e0dc8049b05d655cabd4999f0c1056512
DELL <DELL@DESKTOP-U71I286>
2024-04-03 18:19:50 +0530

    day_4

---
+++ src/day_4/break_statement.java
@@ -0,0 +1,22 @@
+package day_4;
+
+public class break_statement {
+	public static void main(String[] args) {
+		
+		for(int i=5;i<=10;i++)
+		{
+			System.out.println(i);
+			System.out.println("break here");
+			break;
+			
+		}
+		
+		
+		
+	}
+
+	
+	
+	
+	
+}
